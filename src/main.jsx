af8f56c6330e8be368750d7bf97926a77ce4bba3
ines0123 <sametines@insat.u-carthage.tn>
2024-04-01 18:37:52 +0100

    Change from merge

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { registerLicense } from "@syncfusion/ej2-base";
 import { BrowserRouter as Router } from "react-router-dom";
 import WindowContext from "./Context/WindowContext";
 import MenuContext from "./Context/MenuContext";
+import SessionsList from './components/SessionsList/SessionsList.jsx'
 
 registerLicense(
   "ORg4AjUWIQA/Gnt2VVhhQlFaclhJWHxMYVF2R2FJeFRycF9FaEwgOX1dQl9hSXpTcEVmWn9feHVRQWY="
