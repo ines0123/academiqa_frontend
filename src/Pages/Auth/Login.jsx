04ee30176220c498033ef76bdf20a36ba277d0ea
= <mohamedaziz.benghorbel@insat.ucar.tn>
2024-04-24 10:04:50 +0100

    reload page context update

--- src/Pages/Auth/Login.jsx
+++ src/Pages/Auth/Login.jsx
@@ -69,7 +69,7 @@ export default function Login() {
                     // cookie
                     cookie.set('academiqa', token, {
                         path: '/',
-                        httpOnly: true
+                        // httpOnly: true
 
                     });
 
