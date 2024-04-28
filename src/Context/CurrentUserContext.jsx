e96280a8b36b8e1999982f5611aec3b7c54e6007
Rymjbeli <rim.jbeli@insat.ucar.tn>
2024-04-28 17:38:14 +0100

    Some changes

--- src/Context/CurrentUserContext.jsx
+++ src/Context/CurrentUserContext.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Cookie from "cookie-universal";
 import {jwtDecode} from "jwt-decode";
 import axios from "axios";
+import {useNavigate} from "react-router-dom";
 
 export const CurrentUser = createContext({
     currentUser: null,
